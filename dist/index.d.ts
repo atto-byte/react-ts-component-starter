b14e17ffb692dec2af7afe71e1b9389572f71a0d
William Luke <william@ordino.ai>
2019-03-04 22:53:02 +0300

    types

---
+++ dist/index.d.ts
@@ -0,0 +1,11 @@
+import * as React from "react";
+import "./style.css";
+interface IProps {
+}
+interface IState {
+}
+export default class MyComponent extends React.Component<IProps, IState> {
+    constructor(props: IProps);
+    render(): JSX.Element;
+}
+export {};
